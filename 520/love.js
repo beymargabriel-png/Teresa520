09545f13c8356885548a5f8f2178308de79eb93c
beymargabriel-png <beymargabriel@gmail.com>
2026-02-14 18:03:58 -0400

    Update love.js

--- 520/love.js
+++ 520/love.js
@@ -174,7 +174,7 @@
     },
     clear: function () {
       var ctx = this.tree.ctx, cirle = this.cirle;
-      var point = cirle.point, scale = cirle.scale, radius = 26;
+      var point = cirle.point, scale = cirle.scale, radius = 200;
       var w = h = (radius * scale);
       ctx.clearRect(point.x - w, point.y - h, 4 * w, 4 * h);
     },
